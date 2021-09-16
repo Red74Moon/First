109a14dd8534f8db660e1b88e075da1857d775b1
Red74Moon <fedoormaster@gmail.com>
2021-09-16 17:32:46 +0900

    처음 코딩

---
+++ First/First.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyGold = 0;
+	float HP = 0.112122f;
+	char H = 'H';
+	char e = 'e';
+	char l = 'l';
+	char o = 'o';
+
+	cout << MyGold << endl;
+	cout << HP << endl;
+	cout << H << e << l << l << o << endl;
+
+	return 0;
+}
+
